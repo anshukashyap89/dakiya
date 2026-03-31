7526df8be61f5ce406c3fc877cb597f220d1af76
Anshu kashyap <anshu898630@gmail.com>
2026-03-31 22:42:38 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://dakiya-qhel.onrender.com",
     credentials:true
 }))
 app.use(express.json())
